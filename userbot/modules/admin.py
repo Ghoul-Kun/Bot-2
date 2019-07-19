28c989d4a4cf04b45699fffe969c9cb51192c7a2
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2019-07-19 20:45:04 +0530

    Update admin.py

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -7,6 +7,7 @@ Userbot module to help you manage a group
 """
 
 from asyncio import sleep
+from os import remove
 
 from telethon.errors import (BadRequestError, ChatAdminRequiredError,
                              ImageProcessFailedError, PhotoCropSizeSmallError,
